552fd8fdd33c02dd00f562d9db5c4cc103b83e79
Krishna Sagar R <ragas.sagar.474@gmail.com>
2018-12-28 10:39:30 +0000

    replace lodash `get` wtih `getActionPayloadName`

--- src/reducer.js
+++ src/reducer.js
@@ -2,27 +2,32 @@ import get from 'lodash/get';
 import update from 'immutability-helper';
 import { types, generateType } from './';
 
+const getActionPayloadName = (action) => {
+  if(!action || !action.payload ) return;
+  return action.payload.name; 
+};
+
 // this reducer handles all state changes for the uiState slice
 export default function reducer(state = {}, action) {
     // add initial state
-    if (action.type === generateType(types.add, get(action, 'payload.name'))) {
+    if (action.type === generateType(types.add, getActionPayloadName(action))) {
         return { ...state, [action.payload.name]: action.payload.state };
     }
 
     // delete state
-    if (action.type === generateType(types.delete, get(action, 'payload.name'))) {
+    if (action.type === generateType(types.delete, getActionPayloadName(action))) {
         const tempState = { ...state };
         delete tempState[action.payload.name];
         return tempState;
     }
 
     // reset to initial state
-    if (action.type === generateType(types.reset, get(action, 'payload.name'))) {
+    if (action.type === generateType(types.reset, getActionPayloadName(action))) {
         return { ...state, [action.payload.name]: action.payload.state };
     }
 
     // shallow merge for state updates
-    if (action.type === generateType(types.set, get(action, 'payload.name'))) {
+    if (action.type === generateType(types.set, getActionPayloadName(action))) {
         return update(
             state,
             // if store not created with combinedReducers, assume state is top-level
